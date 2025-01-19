8ee126cd5a02fbfa6c52d1582f1d8ce4cea88126
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2025-01-19 22:00:43 +0300

    feat: implement task_3 displaying information about files stored in a folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,32 @@
+const { stdout, stderr } = process;
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+const fsPromises = fs.promises;
+const folderPath = path.join(__dirname, "secret-folder");
+
+fs.access(folderPath, fs.constants.R_OK | fs.constants.W_OK, (error) => {
+  if (error) {
+    stderr.write(`Error: ${error.message}`);
+  } else {
+    fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        stderr.write(`Error: ${error.message}`);
+      } else {
+        for (const file of files) {
+          if (file.isFile()) {
+            const extension = path.extname(file.name);
+            const name = path.basename(file.name, extension);
+            fs.stat(path.join(folderPath, file.name), (error, stats) => {
+              if (error) {
+                stderr.write(`Error: ${error.message}`);
+              } else {
+                stdout.write(`${name} - ${extension.slice(1)} - ${stats.size}b\n`);
+              }
+            });
+          } 
+        }
+      }
+    });
+  }
+});
